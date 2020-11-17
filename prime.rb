30012b46eca210898e8b16e616852191e3dd8afd
Tara Sabella <tarasabella@gmail.com>
2020-11-17 21:13:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+#prime numbers have only 2 factors: divisible by 1 and itself 
+
+require "pry"
+def prime?(int)
+  return false if int <= 1
+  (2..(int-1)).each do |number|
+    if int % number == 0
+      return false
+    end
+    binding.pry
+  end
+  return true
+end
