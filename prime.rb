47e8b98efdba9e132106aae4b80c9407a1f0addd
Tara Sabella <tarasabella@gmail.com>
2020-11-17 21:16:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(int)
   return false if int <= 1
   # (2..(int-1)).each do |number|
-    (2..(int).each do |number|
+    (2..int).each do |number|
 
     if int % number == 0
       return false
