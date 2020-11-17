d985894c5556b6e89b9df51cfff0d8f236da8021
Tara Sabella <tarasabella@gmail.com>
2020-11-17 21:24:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
 #prime numbers have only 2 factors: divisible by 1 and itself 
-#lowest prime number is 2 so anything 1 or less is false 
+#smallest prime number is 2 so anything 1 or less is false 
+# create range starting with 2 (int-1) and create an array 
+#iterate through each item in array 
+#if there is no remainder when integer/item in array then the integer is not prime because it is divisible by more than 2 factors 
 
 def prime?(int)
   return false if int <= 1
